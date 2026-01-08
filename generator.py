946f2c80dcc4005ff7dcedc1c0073e6b37d30b06
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-08 17:12:20 +0100

    Added checks for password requirements

--- generator.py
+++ generator.py
@@ -1,16 +1,9 @@
 import random
 
-# 8 characters
-# 1 uppercase
-# 1 lowercase
-# 1 number
-# 1 special character
-
-
 letters_lower = "abcdefghijklmnopqrstuvwxyz"
 letters_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "1234567890"
 special_chars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+numbers = "1234567890"
 list_of_actions = [letters_lower, letters_upper, numbers, special_chars]
 
 # print(letters_lower[random.randint(0, len(letters_lower)-1)])
@@ -25,7 +18,32 @@ def generate(length):
         list_choice_choice = random.choice(list_choice)
         password += list_choice_choice
         i += 1
-    return password
+
+    has_lower = False
+    has_upper = False
+    has_special = False
+    has_number = False
+
+    for character in password:
+        if character in letters_lower:
+            has_lower = True
+        elif character in letters_upper:
+            has_upper = True
+        elif character in special_chars:
+            has_special = True
+        elif character in numbers:
+            has_number = True
+
+    if (
+        has_lower == True
+        and has_upper == True
+        and has_special == True
+        and has_number == True
+    ):
+        print("Password meets requirements!")
+        return password
+    else:
+        print("Password does not meet requirements!")
 
 
 x = generate(3)
